22823d9209af7c3193c5575014c15ddd9857fcfa
Madeline <1906931+mlizbeth@users.noreply.github.com>
2021-05-25 15:47:42 -0500

    Update Asset_Search.sql

--- asset/Asset_Search.sql
+++ asset/Asset_Search.sql
@@ -7,6 +7,6 @@ from
 ASSET_HISTORY
 join ASSET on ASSET.ID = ASSET_HISTORY.ASSET_ID
 where
-ASSET.NAME = ''
+ASSET.NAME = '' #Device Serial #
 and
-ASSET_HISTORY.FIELD_NAME like '%Name%'
+((ASSET_HISTORY.FIELD_NAME like '%Name%') or (ASSET_HISTORY.FIELD_NAME like '%Archive%'))
